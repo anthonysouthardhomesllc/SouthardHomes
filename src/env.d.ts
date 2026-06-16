94b3ee5e717e8a0d2d18a73d07cde70820bad269
Anthony <anthony.southardhomesllc@gmail.com>
2026-06-16 11:21:26 -0400

    Security Fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-PUBLIC_CLOUDINARY_CLOUD_NAME="dqfotf1jf"
+interface ImportMetaEnv {
+  readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+  readonly PUBLIC_CLOUDINARY_API_KEY: string;
+  readonly CLOUDINARY_API_SECRET: string;
+  readonly PUBLIC_WEB3FORMS_KEY: string;
+}
 
-PUBLIC_CLOUDINARY_API_KEY="117527372545423"
-CLOUDINARY_API_SECRET="LuH9dHjb_REc6n5A__hDrPCEk-U"
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
